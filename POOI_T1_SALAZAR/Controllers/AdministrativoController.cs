73e42dae10c0c6c48542030fff37fff14947ebf0
Franko Raúl Salazar Novoa <frasano.94@gmail.com>
2026-09-22 23:42:16 -0500

    Pregunta_2

---
+++ POOI_T1_SALAZAR/Controllers/AdministrativoController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using POOI_T1_SALAZAR.Models;
+
+namespace POOI_T1_SALAZAR.Controllers
+{
+    public class AdministrativoController : Controller
+    {
+        //Registra al Administrativo
+        [HttpGet]
+        public ActionResult RegistrarAdministrativo()
+        {
+            return View(new Administrativo());
+        }
+
+        //Registra al Administrativo y calcula resultados
+        [HttpPost]
+        public ActionResult RegistrarAdministrativo(Administrativo obj)
+        {
+            return View(obj);
+        }
+    }
+}
---
+++ POOI_T1_SALAZAR/Models/Administrativo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace POOI_T1_SALAZAR.Models{
+    //Administrativo hereda de Empleado
+    public class Administrativo : Empleado{
+        [Display(Name = "Año de Ingreso")]
+        public int AnioIngreso{get; set;}
+
+        [Display(Name = "¿Tiene PostGrado?")]
+        public bool PostGrado{get; set;}
+
+        //Incentivo por PostGrado
+        public double Incentivo(){
+            return PostGrado ? 500.0 : 0.0;
+        }
+
+        // Polimorfismo / Sobrescritura de la bonificación según años de servicio
+        public new double Bonificacion()
+        {
+            int aniosServicio = DateTime.Now.Year - AnioIngreso;
+
+            if (aniosServicio < 5)
+                return 200.0;
+            else if (aniosServicio >= 5 && aniosServicio <= 10)
+                return 450.0;
+            else
+                return 300.0;
+        }
+
+        // Nuevo cálculo total sumando el incentivo
+        public new double MontoAPagar()
+        {
+            return SueldoBasico() + Escolaridad() + Bonificacion() + Incentivo();
+        }
+    }
+}
